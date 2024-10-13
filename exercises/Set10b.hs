4553392dab245045831341390476f8b8e3cc48d8
Milko Burkard <104150789+Milko-B@users.noreply.github.com>
2024-10-13 21:11:47 +0200

    Solved Set 10b (#11)

--- exercises/Set10b.hs
+++ exercises/Set10b.hs
@@ -22,7 +22,8 @@ import Mooc.Todo
 --   False ||| undefined ==> an error!
 
 (|||) :: Bool -> Bool -> Bool
-x ||| y = todo
+x ||| True  = True
+x ||| False = x
 
 ------------------------------------------------------------------------------
 -- Ex 2: Define the function boolLength, that returns the length of a
@@ -36,7 +37,8 @@ x ||| y = todo
 --   length [False,undefined] ==> 2
 
 boolLength :: [Bool] -> Int
-boolLength xs = todo
+boolLength []      = 0
+boolLength (x :xs) = if x then 1 + boolLength xs else 1 + boolLength xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Define the function validate which, given a predicate and a
@@ -50,7 +52,7 @@ boolLength xs = todo
 --   validate (\x -> undefined) 3  ==>  an error!
 
 validate :: (a -> Bool) -> a -> a
-validate predicate value = todo
+validate predicate value = if predicate value then value else value
 
 ------------------------------------------------------------------------------
 -- Ex 4: Even though we can't implement the generic seq function
@@ -84,10 +86,11 @@ class MySeq a where
   myseq :: a -> b -> b
 
 instance MySeq Bool where
-  myseq = todo
+  myseq bool x = if bool then x else x
 
 instance MySeq Int where
-  myseq = todo
+  myseq int x = if even int then x else x
 
 instance MySeq [a] where
-  myseq = todo
+  myseq [] x           = x
+  myseq (first:rest) x = x
